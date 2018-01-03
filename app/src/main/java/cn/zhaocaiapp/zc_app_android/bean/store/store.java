766a5af0353eeee66c5b2796735ef37a30aae6a7
林子 <junhui.zhang@htwins.com.cn>
2018-01-03 11:24:28 +0800

    feature#0 新增实体类

---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/bean/store/store.java
@@ -0,0 +1,9 @@
+package cn.zhaocaiapp.zc_app_android.bean.store;
+
+/**
+ * @author 林子
+ * @filename store.java
+ * @data 2018-01-03 11:21
+ */
+public class store {
+}
---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/bean/user/user.java
@@ -0,0 +1,9 @@
+package cn.zhaocaiapp.zc_app_android.bean.user;
+
+/**
+ * @author 林子
+ * @filename user.java
+ * @data 2018-01-03 11:23
+ */
+public class user {
+}
