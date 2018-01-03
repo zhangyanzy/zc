84a1affa08e9a6d337675990566f206a7c248c11
aiwen <123@qq.com>
2018-01-03 10:31:58 +0800

    添加工具类

---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/common/constant/Constants.java
@@ -0,0 +1,13 @@
+package cn.zhaocaiapp.zc_app_android.common.constant;
+
+/**
+ * 应用常量
+ *
+ * Created by jinxunmediapty.ltd on 2018/1/3.
+ */
+
+public class Constants {
+
+    //toast提示相关
+    public final static String TOAST_COMMON_ERROR = "失败";
+}
--- app/src/main/java/cn/zhaocaiapp/zc_app_android/util/ToastUtil.java
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/util/ToastUtil.java
@@ -1,6 +1,9 @@
 package cn.zhaocaiapp.zc_app_android.util;
 
+import android.content.Context;
 import android.widget.Toast;
+
+import cn.zhaocaiapp.zc_app_android.common.constant.Constants;
 
 /**
  * 提示
@@ -11,6 +14,60 @@ import android.widget.Toast;
 public class ToastUtil {
 
     private static Toast toast;
+
+    /**
+     * 显示toast提示
+     *
+     * @param context
+     * @param msg
+     *
+     */
+    public static void makeText(Context context, String msg) {
+
+        if (toast == null) {
+            toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(msg);
+        }
+        toast.show();
+    }
+
+    /**
+     * 显示toast提示
+     *
+     * @param context
+     * @param msg
+     *
+     */
+    public static void makeTextLong(Context context, String msg) {
+
+        if (toast == null) {
+            toast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
+        } else {
+            toast.setText(msg);
+        }
+        toast.show();
+    }
+
+    /**
+     * 显示失败信息
+     *
+     * @param context
+     *
+     */
+    public static void makeTextError(Context context) {
+        makeText(context, Constants.TOAST_COMMON_ERROR);
+    }
+
+    /**
+     * 显示失败信息
+     *
+     * @param context
+     *
+     */
+    public static void makeTextErrorLong(Context context) {
+        makeTextLong(context, Constants.TOAST_COMMON_ERROR);
+    }
 
     /**
      * 销毁
