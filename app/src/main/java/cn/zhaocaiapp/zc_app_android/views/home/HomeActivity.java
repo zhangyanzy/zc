98f9994685b9fdbdade3e3e343b4407087494fdb
林子 <junhui.zhang@htwins.com.cn>
2018-01-05 17:57:31 +0800

    feature#0 创建文件夹

---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/views/home/HomeActivity.java
@@ -0,0 +1,9 @@
+package cn.zhaocaiapp.zc_app_android.views.home;
+
+/**
+ * @author 林子
+ * @filename HomeActivity.java
+ * @data 2018-01-05 17:52
+ */
+public class HomeActivity {
+}
---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/views/login/LoginActivity.java
@@ -0,0 +1,9 @@
+package cn.zhaocaiapp.zc_app_android.views.login;
+
+/**
+ * @author 林子
+ * @filename LoginActivity.java
+ * @data 2018-01-05 17:52
+ */
+public class LoginActivity {
+}
