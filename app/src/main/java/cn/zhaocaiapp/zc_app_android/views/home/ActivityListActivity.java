c41295cc35308f0ff9ac2b0003a075711ce867f8
aiwen <123@qq.com>
2018-01-08 15:36:38 +0800

    首页 tab页

---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/views/home/ActivityListActivity.java
@@ -0,0 +1,22 @@
+package cn.zhaocaiapp.zc_app_android.views.home;
+
+import android.os.Bundle;
+
+import cn.zhaocaiapp.zc_app_android.R;
+import cn.zhaocaiapp.zc_app_android.base.BaseActivity;
+
+/**
+ * 活动列表
+ */
+public class ActivityListActivity extends BaseActivity {
+
+    @Override
+    public int getContentViewResId() {
+        return R.layout.activity_list;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
