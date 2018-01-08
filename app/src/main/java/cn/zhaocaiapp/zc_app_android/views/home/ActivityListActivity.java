a5232593ca51f67e42be5fb25c9057fc8615e406
aiwen <123@qq.com>
2018-01-08 17:22:44 +0800

    首页 tab页

--- app/src/main/java/cn/zhaocaiapp/zc_app_android/views/home/ActivityListActivity.java
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/views/home/ActivityListActivity.java
@@ -1,7 +1,16 @@
 package cn.zhaocaiapp.zc_app_android.views.home;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.BindView;
 import cn.zhaocaiapp.zc_app_android.R;
 import cn.zhaocaiapp.zc_app_android.base.BaseActivity;
 
@@ -10,6 +19,17 @@ import cn.zhaocaiapp.zc_app_android.base.BaseActivity;
  */
 public class ActivityListActivity extends BaseActivity {
 
+    @BindView(R.id.active_list) ListView listView;
+    private List<String> activityList;
+
+    {
+        activityList.add("1");
+        activityList.add("2");
+        activityList.add("3");
+        activityList.add("4");
+        activityList.add("5");
+    }
+
     @Override
     public int getContentViewResId() {
         return R.layout.activity_list;
@@ -17,6 +37,34 @@ public class ActivityListActivity extends BaseActivity {
 
     @Override
     public void init(Bundle savedInstanceState) {
+        listView.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return activityList.size();
+            }
 
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = null;
+
+                LayoutInflater inflater = ActivityListActivity.this.getLayoutInflater();
+                view = inflater.inflate(R.layout.activity_activity_scene_detail,null);
+
+                TextView textView = view.findViewById(R.id.textView);
+                textView.setText(activityList.get(position));
+
+                return view;
+            }
+        });
     }
 }
