944680b1b612f0bfdbf290f1eaa6647d53ef317e
aiwen <123@qq.com>
2018-01-10 16:36:23 +0800

    h5内嵌

--- app/src/main/java/cn/zhaocaiapp/zc_app_android/TestFragment.java
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/TestFragment.java
@@ -1,5 +1,6 @@
 package cn.zhaocaiapp.zc_app_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +12,8 @@ import android.widget.Button;
 import butterknife.BindView;
 import butterknife.OnClick;
 import cn.zhaocaiapp.zc_app_android.base.BaseFragment;
+import cn.zhaocaiapp.zc_app_android.util.ToastUtil;
+import cn.zhaocaiapp.zc_app_android.views.activity.ActivityActivityDetail;
 import cn.zhaocaiapp.zc_app_android.views.login.LoginActivity;
 
 /**
@@ -22,9 +25,28 @@ public class TestFragment extends BaseFragment {
     @BindView(R.id.button1)
     Button button1;
 
+    Button goH5;
+
     @Override
     public View setContentView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.test_main, container, false);
+        View view = inflater.inflate(R.layout.test_main, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //h5页面
+        goH5 = (Button) getActivity().findViewById(R.id.goH5);
+        goH5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastUtil.makeText(getContext(),"跑到这里了");
+                Intent intent =new Intent(getActivity(),ActivityActivityDetail.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
