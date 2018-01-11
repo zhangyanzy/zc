36a1ca8a65fb6b5d31e66a18804d6a20ec5478bb
林子 <junhui.zhang@htwins.com.cn>
2018-01-11 15:24:34 +0800

    Revert:  商家列表

--- app/src/main/java/cn/zhaocaiapp/zc_app_android/WelcomeActivity.java
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/WelcomeActivity.java
@@ -97,20 +97,20 @@ public class WelcomeActivity extends AppCompatActivity {
     private void addBottomDots(int currentPage) {
         dots = new TextView[layouts.length];
 
-        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
-        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
+        //int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        //int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
 
         dotsLayout.removeAllViews();
         for (int i = 0; i < dots.length; i++) {
             dots[i] = new TextView(this);
             dots[i].setText(Html.fromHtml("&#8226;"));//圆点
             dots[i].setTextSize(35);
-            dots[i].setTextColor(colorsInactive[currentPage]);
+            //dots[i].setTextColor(colorsInactive[currentPage]);
             dotsLayout.addView(dots[i]);
         }
 
         if (dots.length > 0) {
-            dots[currentPage].setTextColor(colorsActive[currentPage]);
+            //dots[currentPage].setTextColor(colorsActive[currentPage]);
         }
     }
 
