ef2151b7dc9c79a7054df637fd426e8fc9a537c2
林子 <junhui.zhang@htwins.com.cn>
2018-01-15 15:30:53 +0800

    Revert:  集成高德地图

---
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/views/home/LocationActivity.java
@@ -0,0 +1,22 @@
+package cn.zhaocaiapp.zc_app_android.views.home;
+
+import android.os.Bundle;
+
+import cn.zhaocaiapp.zc_app_android.base.BaseActivity;
+
+/**
+ * @author 林子
+ * @filename LocationActivity.java
+ * @data 2018-01-15 13:36
+ */
+public class LocationActivity extends BaseActivity {
+    @Override
+    public int getContentViewResId() {
+        return 0;
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
