1716c5c11062b75a03b9fa39f83220c868a8184c
李海鹏 <lihaipeng@zhaocaiapp.cn>
2018-01-22 19:41:20 +0800

    #feature0 导入切图

--- app/src/main/java/cn/zhaocaiapp/zc_app_android/TestFragment.java
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/TestFragment.java
@@ -3,7 +3,6 @@ package cn.zhaocaiapp.zc_app_android;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
