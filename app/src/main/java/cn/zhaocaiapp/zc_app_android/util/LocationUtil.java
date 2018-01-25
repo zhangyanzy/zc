1dbc4d4d085acd5fdfe8916db60eb8add64b92b4
林子 <junhui.zhang@htwins.com.cn>
2018-01-25 15:13:10 +0800

    Revert: 定位

--- app/src/main/java/cn/zhaocaiapp/zc_app_android/util/LocationUtil.java
+++ app/src/main/java/cn/zhaocaiapp/zc_app_android/util/LocationUtil.java
@@ -38,6 +38,7 @@ public class LocationUtil {
         locationClient.setLocationListener(locationListener);
         locationClient.startLocation();
 
+        gps = new Gps();
         gps.setOpen(false); //默认未定位
     }
 
@@ -51,7 +52,7 @@ public class LocationUtil {
         mOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Battery_Saving);//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
         mOption.setGpsFirst(false);//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
         mOption.setHttpTimeOut(30000);//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        mOption.setInterval(3000);//可选，设置定位间隔。默认为2秒
+        mOption.setInterval(300000);//可选，设置定位间隔。默认为2秒
         mOption.setNeedAddress(true);//可选，设置是否返回逆地理地址信息。默认是true
         mOption.setOnceLocation(false);//可选，设置是否单次定位。默认是false
         mOption.setOnceLocationLatest(false);//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
@@ -68,7 +69,6 @@ public class LocationUtil {
     static AMapLocationListener locationListener = new AMapLocationListener() {
         @Override
         public void onLocationChanged(AMapLocation location) {
-            Gps gps = new Gps();
             if (null != location) {
 
                 StringBuffer sb = new StringBuffer();
